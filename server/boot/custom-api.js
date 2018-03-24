2811d265320f393cb07808a22cd226e5c5414634
Danang Aji Tamtomo <imsuretamtomo@gmail.com>
2018-03-24 21:50:25 +0700

    add account creation and user register api

--- server/boot/custom-api.js
+++ server/boot/custom-api.js
@@ -1,16 +1,16 @@
 'use strict';
 const axios = require('axios');
+const API_KEY = 'FMTYqeTayozJiH2PNcN3Y2nTqGfVKKoK';
+const requestConfig = {
+  headers: {
+    apikey: API_KEY
+  }
+}
+const HOUSE_LIST_API_URL = 'https://mortgtech-eval-prod.apigee.net/btn-mortgtech/house-list'
 
 module.exports = function(server) {
   // Install a `/` route that returns server status
   const router = server.loopback.Router();
-  const API_KEY = 'FMTYqeTayozJiH2PNcN3Y2nTqGfVKKoK'
-  const HOUSE_LIST_API_URL = 'https://mortgtech-eval-prod.apigee.net/btn-mortgtech/house-list'
-  const requestConfig = {
-    headers: {
-      apikey: API_KEY
-    }
-  }
 
   const getHouseListController = async (req, res) => {
     const { keyword } = req.query
@@ -40,7 +40,82 @@ module.exports = function(server) {
     }
   }
 
+  const postUserRegister = async (req, res) => {
+    const CREATE_USER_API = 'https://mortgtech-eval-prod.apigee.net/btn-mortgtech/user-register'
+    const {
+      nik,
+      nama,
+      tgl_lahir,
+      nama_ibu_kandung
+    } = req.query
+    try {
+      const { data: { payload } } = await axios.post(CREATE_USER_API, {
+        nik,
+        nama,
+        tgl_lahir,
+        nama_ibu_kandung
+      }, requestConfig)
+
+      if (payload) {
+        res
+          .status(200)
+          .send({
+            status: 200,
+            message: 'Create User Success',
+            payload
+          })
+      }
+
+    } catch (err) {
+      console.log(err);
+      res
+        .status(400)
+        .send({
+          status: 400,
+          message: 'Something wrong with your request',
+          error: err.message
+        })
+    }
+  }
+
+  const accountCreation = async (req, res) => {
+    const CREATE_USER_API = 'https://mortgtech-eval-prod.apigee.net/btn-mortgtech/account-creation'
+    const {
+      nomor_cif,
+      amount
+    } = req.query
+    
+    try {
+      const { data: { payload } } = await axios.post(CREATE_USER_API, {
+        nomor_cif,
+        amount: parseInt(amount, 10)
+      }, requestConfig)
+
+      if (payload) {
+        res
+          .status(200)
+          .send({
+            status: 200,
+            message: 'Account Creation Success',
+            payload
+          })
+      }
+
+    } catch (err) {
+      console.log(err);
+      res
+        .status(400)
+        .send({
+          status: 400,
+          message: 'Something wrong with your request',
+          error: err.message
+        })
+    }
+  }
+
   router.get('/api/house-list', getHouseListController);
+  router.get('/api/user-register', postUserRegister);
+  router.get('/api/account-creation', accountCreation);
 
   server.use(router);
 };
