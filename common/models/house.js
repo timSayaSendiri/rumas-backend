a416ac96e4b1a61cd9c150834767238760720013
dikyarga <dikyarga.id@gmail.com>
2018-03-24 14:36:40 +0700

    create test api to apigee

--- common/models/house.js
+++ common/models/house.js
@@ -1,5 +1,30 @@
 'use strict';
 
+let axios = require('axios');
 module.exports = function(House) {
+  House.testBTNAPI = function(cb) {
+    axios.post(process.env.BTN_API + '/house-list', {
+      headers: {
+        apikey: process.env.BTN_TOKEN,
+        'Content-Type': 'application/json',
+      },
+    }).then(res => {
+      console.log('res : ', res)
+      cb(null, res)
+    }).catch(err => {
+      console.log('error : ', err)
+    })
+  }
+
+  House.remoteMethod('testBTNAPI', {
+    http: {
+      path: '/test-btn-api',
+      verb: 'get',
+    },
+    returns: {
+      arg: 'result', type: 'object',
+    },
+    description: 'just test BTN API',
+  })
 
 };
